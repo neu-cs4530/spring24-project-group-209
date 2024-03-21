d313cdc7033d3f6a01abf78cc0e380e15fd401ca
JarodD19 <113318528+JarodD19@users.noreply.github.com>
2024-03-20 21:11:14 -0400

    Database and authentication framework

--- shared/firebaseInit.ts
+++ shared/firebaseInit.ts
@@ -1,9 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from 'firebase/firestore';
+import { DocumentData, DocumentReference, arrayUnion, doc, getDoc, getFirestore, setDoc, updateDoc } from 'firebase/firestore';
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { useToast } from "@chakra-ui/react";
+import activeUsers from "../frontend/src/activeUsers.json";
+import { set } from "ramda";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,23 +21,22 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const toast = useToast();
+export const auth = getAuth();
 
 const handleJoin = (userName: string, pass: string) => {
-  const toast = useToast();
-  const auth = getAuth();
-  const db = getFirestore();
   createUserWithEmailAndPassword(auth, userName, pass)
-    .then((userCredential) => {
-      
+    .then(async (userCredential: { user: any; }) => {
+
       const user = userCredential.user;
-      return db.collection("users").doc(user).set({
-        currency : 0,
-        inventory : [],
+      await setDoc(doc(db, "users", user), {
+        currency: 0,
+        inventory: [],
       })
 
-      const curUser = { userName: user };
+      const curUser = { "username": userName, "credential": user };
       const fs = require("fs");
       let usersJson = fs.readFileSync("activeUsers.json", "utf-8");
       let users = JSON.parse(usersJson);
@@ -44,15 +44,14 @@ const handleJoin = (userName: string, pass: string) => {
       usersJson = JSON.stringify(users);
       fs.writeFileSync("activeUsers.json", usersJson, "utf-8");
     })
-    .catch((error) => {
+    .catch((error: { code: any; message: any; }) => {
       const errorCode = error.code;
       const errorMessage = error.message;
       if (errorCode == 'auth/email-already-in-use') {
         signInWithEmailAndPassword(auth, userName, pass)
-          .then((userCredential) => {
+          .then((userCredential: { user: any; }) => {
             // Signed in 
             const user = userCredential.user;
-
             const curUser = { userName: user };
             const fs = require("fs");
             let usersJson = fs.readFileSync("activeUsers.json", "utf-8");
@@ -62,7 +61,7 @@ const handleJoin = (userName: string, pass: string) => {
             fs.writeFileSync("activeUsers.json", usersJson, "utf-8");
             // ...
           })
-          .catch((error) => {
+          .catch((error: { code: any; message: any; }) => {
             const errorCode = error.code;
             const errorMessage = error.message;
             toast({
@@ -76,3 +75,71 @@ const handleJoin = (userName: string, pass: string) => {
       // ..
     });
 }
+
+const getCurrency = async (userName: string) => {
+  const cred = activeUsers.find((user: { username: string; }) => user.username === userName);
+  if (cred) {
+    const docRef = doc(db, "users", cred.credential);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      return docSnap.data().currency;
+    }
+    else {
+      throw new Error("User data not found");
+    }
+  }
+  else {
+    throw new Error("User not found");
+  }
+}
+// Option for updating currency based on local total being tracked and then updating
+const updateCurrency = (userName: string, newValue : number) => {
+  const cred = activeUsers.find((user: { username: string; }) => user.username === userName);
+  if (cred) {
+    const userRef = doc(db, "users", cred.credential);
+    updateDoc(userRef, {currency: newValue}); 
+  }
+  else {
+    throw new Error("User not found");
+  }
+ }
+// Option for updating currency based on running net total earned/lossed in session
+//  const updateCurrency = (userName: string, newValue : number) => {
+//   const cred = activeUsers.find((user: { username: string; }) => user.username === userName);
+//   if (cred) { 
+//     const userRef = doc(db, "users", cred.credential);
+//     updateDoc(userRef, {currency: increment(newValue)}); 
+//   }
+//   else {
+//     throw new Error("User not found");
+//   }
+//  }
+
+ const getInventory = async (userName: string) => {
+  const cred = activeUsers.find((user: { username: string; }) => user.username === userName);
+  if (cred) {
+    const docRef = doc(db, "users", cred.credential);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      return docSnap.data().inventory;
+    }
+    else {
+      throw new Error("User data not found");
+    }
+  }
+  else {
+    throw new Error("User not found");
+  }
+}
+
+const updateInventory =  (userName: string, newValue : number) => {
+  const cred = activeUsers.find((user: { username: string; }) => user.username === userName);
+  if (cred) {
+    const userRef = doc(db, "users", cred.credential);
+    updateDoc(userRef, {inventory: arrayUnion(newValue)}); 
+  }
+  else {
+    throw new Error("User not found");
+  }
+ }
+
